3778f761b83f6619d318b7a72dd135bc6ef91752
Halil İbrahim ŞAFAK <halil.safak@pupabilisim.com.tr>
2015-03-14 17:04:47 +0200

    file structure changed

---
+++ Assets/Scripts/ResizeCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class ResizeCamera : MonoBehaviour 
+{
+	private float   DesignOrthographicSize;
+	private float   DesignAspect;
+	private float   DesignWidth;
+	
+	public  float   DesignAspectHeight;
+	public  float   DesignAspectWidth;
+	
+	public void Awake()
+	{
+		this.DesignOrthographicSize = this.camera.orthographicSize;
+		this.DesignAspect = this.DesignAspectHeight / this.DesignAspectWidth;
+		this.DesignWidth = this.DesignOrthographicSize * this.DesignAspect;
+		
+		this.Resize();
+	}
+	
+	public void Resize()
+	{       
+		float wantedSize = this.DesignWidth / this.camera.aspect;
+		this.camera.orthographicSize = Mathf.Max(wantedSize, 
+		                                         this.DesignOrthographicSize);
+	}
+}
